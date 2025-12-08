b8e2b0192ac32102c1ada7b17ddeec8cba541e62
Jamaledm <Jamalbazan@gmail.com>
2025-12-07 23:31:01 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { TurnosProvider } from './context/TurnosContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TurnosProvider>
+        <App />
+      </TurnosProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
